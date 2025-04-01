20b3d1992c8ac893a747bd42e87b824c3f6eb939
Abai369 <jcaleb1110@gmail.com>
2025-03-31 19:33:47 -0700

    fix database connection

--- backend/settings.py
+++ backend/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nombre_de_tu_db',
+        'USER': 'tu_usuario',
+        'PASSWORD': 'tu_contraseña',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
