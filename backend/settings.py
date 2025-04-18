9f6934db97d1ccc39dd5f18c6070a8d09bd5857f
Abai369 <jcaleb1110@gmail.com>
2025-04-17 20:58:02 -0700

    start configuration for django scheduler

--- backend/settings.py
+++ backend/settings.py
@@ -119,3 +119,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INSTALLED_APPS += ['django_celery_beat']
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'  # necesitas Redis
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
