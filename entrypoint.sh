eb38237518e9b10fafbdcaa33bc4e08c9f246abf
Edgar <ebyted@gmail.com>
2025-05-01 09:37:33 -0700

    refactor: update Dockerfile and docker-compose.yml for port changes and remove unused files

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Cargar variables desde .env
-export $(grep -v '^#' .env | xargs)
-
-# Ejecutar migraciones
-python manage.py migrate
-
-# Iniciar servidor gunicorn
-gunicorn backend.wsgi:application --bind 0.0.0.0:8000
