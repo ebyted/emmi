b4b41aa9d695e66bab1a79c697e193109456ffcf
Abai369 <jcaleb1110@gmail.com>
2025-05-01 15:01:13 -0700

    feat: set up environ for environment variable management

--- backend/settings.py
+++ backend/settings.py
@@ -2,8 +2,15 @@ from pathlib import Path
 import os
 import environ
 
+# --- ENVIRON SETUP
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
