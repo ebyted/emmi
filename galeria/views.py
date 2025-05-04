454e7d50a8bf09f5119c0550468e126c5b45450b
Edgar <ebyted@gmail.com>
2025-05-04 00:23:20 -0700

    fix: update galeria_view to render the correct template file

--- galeria/views.py
+++ galeria/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def galeria_view(request):
-    return render(request, 'galeria/galeria.html')  # debes crear este template
+    return render(request, 'galeria/index.html')  # debes crear este template
