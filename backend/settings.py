f314423e0c298bb509aeba1a76b9d11fcb497a8e
Edgar <ebyted@gmail.com>
2025-05-03 20:46:39 -0700

    fix: update database configuration in .env and settings.py for local development

--- backend/settings.py
+++ backend/settings.py
@@ -75,8 +75,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('POSTGRES_DB'),
-        'USER': 'ebyted',
-        'PASSWORD': 'arkano',
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
         'HOST': env('POSTGRES_HOST'),
         'PORT': env('POSTGRES_PORT'),
     }
